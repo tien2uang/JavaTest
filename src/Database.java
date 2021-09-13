f6a79859a132fade5073583f2eaff263d4401d9e
tquang <duche345@gmail.com>
2021-09-13 17:15:52 +0700

    Create some functions

--- src/Database.java
+++ src/Database.java
@@ -1,2 +1,19 @@
 public class Database {
+    public double getAvailableBalance()
+    {
+        return 0.0;
+    }
+    public double getTotalBalance()
+    {
+        return 0.0;
+
+    }
+    public void debit()
+    {
+
+    }
+    public  void credit()
+    {
+
+    }
 }
