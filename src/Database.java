b8943846ebf21928402541e259de372f19fa8a65
tquang <duche345@gmail.com>
2021-09-14 12:16:28 +0700

    Hoàn thành class Database

--- src/Database.java
+++ src/Database.java
@@ -1,23 +1,64 @@
+import java.util.AbstractCollection;
+
 public class Database {
-    public Boolean authenticateUser()
+    private Account[] accounts;
+    public Database()
     {
-        return true;
+        accounts=new Account[2];
+        accounts[0]= new Account("2904",853211,1234.0);
+        accounts[1]= new Account("2002",722766,3422.0);
     }
-    public double getAvailableBalance()
+    public Boolean isAccountExist(String userAccountNumber)
     {
-        return 0.0;
+        for(Account currentAccount : accounts)
+        {
+            if(currentAccount.getAccountNumber().equals(userAccountNumber))
+            {
+                return true;
+            }
+        }
+        return  false;
     }
-    public double getTotalBalance()
+
+    public Account getCurrentAccount(String userAccountNumber)
     {
-        return 0.0;
+        for(Account account:accounts)
+        {
+            if(account.getAccountNumber().equals(userAccountNumber))
+            {
+                return account;
+
+            }
+        }
+        return null;
+    }
+    public Boolean authenticateUser(String userAccountNumber,int userPin)
+    {
+        Boolean check=false;
+        Account currentAccount=getCurrentAccount(userAccountNumber);
+        if(currentAccount!=null)
+        {
+            if(currentAccount.validatePIN(userPin))
+            {
+                check=true;
+            }
+        }
+
+        return check;
+    }
+
+    public double getTotalBalance(String userAccountNumber)
+    {
+        Account currentAccount=getCurrentAccount(userAccountNumber);
+        return currentAccount.getTotalBalance();
 
     }
-    public void debit()
+    public void debit(String userAccountNumber,double amount)
     {
-
+        getCurrentAccount(userAccountNumber).debit(amount);
     }
-    public  void credit()
+    public  void credit(String userAccountNumber,double amount)
     {
-
+        getCurrentAccount(userAccountNumber).credit(amount);
     }
 }
