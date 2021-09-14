9c7bcbd6ebf294a759c374cc66f95b391af8d25d
tquang <duche345@gmail.com>
2021-09-14 12:18:47 +0700

    Hoàn thành 2 class Transaction và Screen

--- src/Screen.java
+++ src/Screen.java
@@ -1,6 +1,15 @@
 public class Screen {
-    public void displayMessage()
+    public void displayLineMessage(String message)
     {
+        System.out.println(message);
 
+    }
+    public void  displayMessage(String message)
+    {
+        System.out.print(message);
+    }
+    public void displayDollarMessage(Double amount)
+    {
+        System.out.printf("%.2f",amount);
     }
 }
--- src/Transaction.java
+++ src/Transaction.java
@@ -1,3 +1,23 @@
-public class Transaction {
+abstract public class Transaction {
+    private String accountNumber;
+    private Database database;
+    private Screen screen;
+    public Transaction(String accountNumber,Database database,Screen screen)
+    {
+        this.accountNumber=accountNumber;
+        this.database=database;
+        this.screen=screen;
+    }
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    abstract public  void  execute();
 
+    public Database getDatabase() {
+        return database;
+    }
+
+    public Screen getScreen() {
+        return screen;
+    }
 }
