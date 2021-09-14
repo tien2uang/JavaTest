aff08ee33bd7d88844106c2ed2348216b97bbc3e
tquang <duche345@gmail.com>
2021-09-14 14:31:19 +0700

    Hoan thanh class CashDispender

--- src/CashDispenser.java
+++ src/CashDispenser.java
@@ -1,11 +1,16 @@
 public class CashDispenser {
     private int count =500;
-    public Boolean isSufficientCashAvailable()
+    public Boolean isSufficientCashAvailable(double amount)
     {
-        return true;
+        if(amount/20<count)
+            return true;
+        else
+            return false;
+
     }
-    public void dispenseCash()
+    public void dispenseCash(double amount)
     {
+        count -=amount/20;
 
     }
 }
